967c9469464f924d898ceb5c5f71a265c7f8629a
tkj <taukhidajin@gmail.com>
2022-12-17 23:12:11 +0700

    dockerfile

---
+++ Doc Docker/basic-networking-submission-docker-taukhid/runnodenginx.sh
@@ -0,0 +1,6 @@
+sudo docker network rm nodapache-net
+sudo killall -9 node
+sudo docker-compose stop
+sudo systemctl restart docker
+sudo docker network create nodnginx-net
+sudo docker-compose up -d
